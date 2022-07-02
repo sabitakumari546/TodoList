7f825ea238dffe75b26d96d65cb79eea255c60d6
Sabita kumari <savita985233@gmail.com>
2022-07-02 23:04:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import Rastaurant from "./components/Basics/Rastaurant";
+import Todo from "./components/todoreact/todo";
+
+const App = () => {
+  return (
+    <>
+  <Todo/>
+  </>)
+};
+
+export default App;
+
